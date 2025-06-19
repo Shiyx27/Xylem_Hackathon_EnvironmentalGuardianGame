d3c9fcf6b22b5bc7e1bd2b5a6dfe8b416b966536
Shiyamaladevi R S <143835823+Shiyx27@users.noreply.github.com>
2025-06-19 18:10:11 +0530

    Delete environmental_game directory

--- environmental_game/run.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    print("🌍 Starting Environmental Guardian Game...")
-    print("📍 Open your browser and go to: http://127.0.0.1:5000")
-    print("🎮 Have fun saving the planet!")
-    app.run(debug=True, host='127.0.0.1', port=5000)
